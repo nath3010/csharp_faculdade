0693369eb0962dabf0b0c5ecbcccfb7b7cd26e1a
Ailton Silva <ailtonsscarvalho@gmail.com>
2018-10-26 23:18:12 -0300

    Adicionado ex2 na solução primaria

---
+++ primeira-aula/PrimeiraAula/Exercicios/Ex2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrimeiraAula.Exercicios
+{
+    public class Ex2
+    {
+        public static void Inicio()
+        {
+            int n1, n2, area;
+            Console.WriteLine("Qual e o valor da base do retângulo?");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual e o valor da altura do retângulo?");
+            n2 = int.Parse(Console.ReadLine()); //Número usa parse.
+
+            area = n1 * n2;
+
+            Console.WriteLine("Área = {0}", area);
+
+            Console.WriteLine("Aperte uma tecla para sair.");
+            Console.ReadKey(); //Para clicar em uma tecla e sair
+        }
+    }
+}
