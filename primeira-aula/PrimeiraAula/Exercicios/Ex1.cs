2c7dc058b486635108e073474a0bd05334da01ed
Ailton Silva <ailtonsscarvalho@gmail.com>
2018-10-26 23:25:06 -0300

    Removido espaços desnecessarios

--- primeira-aula/PrimeiraAula/Exercicios/Ex1.cs
+++ primeira-aula/PrimeiraAula/Exercicios/Ex1.cs
@@ -11,14 +11,11 @@ namespace PrimeiraAula.Exercicios
             string telefone = Entrada.ReadString("Escreva o telefone:");
             string endereco = Entrada.ReadString("Escreva o endereço:");
             string cpf = Entrada.ReadString("Escreva o CPF:");
-
             Console.WriteLine("O nome é: {0}", nome);
             Console.WriteLine("O telefone é: {0}", telefone);
             Console.WriteLine("O endereço é: {0}", endereco);
             Console.WriteLine("O CPF é: {0}", cpf);
-
             Menu.Esperar();
-
         }
     }
 }
--- primeira-aula/PrimeiraAula/Program.cs
+++ primeira-aula/PrimeiraAula/Program.cs
@@ -13,6 +13,7 @@ namespace PrimeiraAula
                 Menu menu = new Menu()
                     .Add("Ex1", () => Ex1.Inicio())
                     .Add("Ex2", () => Ex2.Inicio())
+
                     .Add("Fechar", () => continuar = false);
                 menu.Display();
             } while (continuar);
