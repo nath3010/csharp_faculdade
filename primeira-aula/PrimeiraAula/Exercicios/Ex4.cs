2aacd5a9eb521b0f5744f804da11a477fbd29440
Ailton Silva <ailtonsscarvalho@gmail.com>
2018-10-26 23:44:27 -0300

    Conversao direta

--- primeira-aula/PrimeiraAula/Exercicios/Ex4.cs
+++ primeira-aula/PrimeiraAula/Exercicios/Ex4.cs
@@ -9,13 +9,12 @@ namespace PrimeiraAula.Exercicios
         {
             float raio, perimetro, area;
 
-
             Console.WriteLine("Escreva o raio do círculo:");
             raio = float.Parse(Console.ReadLine());
-            
 
-            perimetro = Convert.ToUInt32((2 * 3.14) * raio);  //Não esquecer de por pnto de não virgula!!
-            area = Convert.ToUInt32((raio * raio) * 3.14);
+
+            perimetro = (float)(2 * 3.14) * raio;  //Não esquecer de por pnto de não virgula!!
+            area = (float)((raio * raio) * 3.14);
 
             Console.WriteLine(" O perímetro é: {0}", perimetro);
             Console.WriteLine("A área é: {0}", area);
